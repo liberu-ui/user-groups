196e8e06a89652a79cd36bd15490527eff7e5271
mishu <mishu@xtelecom.ro>
2021-06-02 08:46:10 +0300

    removes unneded file

--- src/bulma/register.js
+++
@@ -1,6 +0,0 @@
-// import Vue from 'vue';
-// import App from '@enso-ui/ui/src/core/app';
-// import ProfileController from './components/navbar/ProfileControl.vue';
-//
-// Vue.component('navbar-profile-controller', ProfileController);
-// App.registerNavbarItem('navbar-profile-controller', 5);
